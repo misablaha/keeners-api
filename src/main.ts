f5f644ca1c6c3cb9fa0ab19a7a51623e1fc67d2c
Michal Bláha <michal.blaha@timeisltd.com>
2020-04-09 09:58:39 +0200

    Add Sentry

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
+  Sentry.init({ dsn: 'https://62db57ae119448df80034147629d4a0c@o148599.ingest.sentry.io/5194594' });
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: false }));
